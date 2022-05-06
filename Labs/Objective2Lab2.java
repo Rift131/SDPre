9867097be8e6c434ceb5dc188c45341c0801b52d
Ken Pederson <102421691+Rift131@users.noreply.github.com>
2022-05-06 11:16:03 -0500

    Delete Objective2Lab2.java

--- Labs/Objective2Lab2.java
+++
@@ -1,9 +0,0 @@
-public class Objective2Lab2 {
-
-  public static void main(String[] args) {
-    String name = "Ken Pederson";
-    int age = 40;
-    String state = "Nebraska";
-    System.out.print("Name:" + " " + name + " " + "Age:" + " " + age + " " + "State:" + " " + state);
-  }
-}
