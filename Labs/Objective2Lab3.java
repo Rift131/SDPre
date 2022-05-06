964f99f98a5253b5ddf0bb31b29589bd99a466b0
Kenneth Pederson <kenneth.pederson@yahoo.com>
2022-05-06 11:53:37 -0500

    Complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+
+  public static void main(String[] args) {
+  int num1 = 5;
+  double num2 = 10.5;
+  int num3 = 15;
+  double average = (num1 + num2 + num3) / 3;
+  System.out.println(average); 
+  }
+}
