82666d5873a039012bd22747d6e15fdad4a6d940
Kenneth Pederson <kenneth.pederson@yahoo.com>
2022-05-09 15:55:51 -0500

    Complete Objective5Lab1

---
+++ Labs/Objective5Lab1.java
@@ -0,0 +1,15 @@
+public class Objective5Lab1 {
+
+  public static void main(String[] args) {
+  int num1 = 5;
+  int num2 = 15;
+
+    // if else statement for num1 > num2
+    if (num1 > num2) {
+      System.out.println(num1 + " is greater than " + num2);
+    // if else statement for num1 < num2
+  } else if (num2 > num1) {
+      System.out.println(num2 + " is greater than " + num1);
+    }
+  }
+}
