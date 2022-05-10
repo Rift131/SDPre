c622efab672b48632f49da95f7a35b64bdc69e74
Kenneth Pederson <kenneth.pederson@yahoo.com>
2022-05-10 09:02:31 -0500

    Complete Objective2Lab2

--- Labs/Objective2Lab2.java
+++ Labs/Objective2Lab2.java
@@ -4,6 +4,7 @@ public class Objective2Lab2 {
       String name = "Ken Pederson";
       int age = 40;
       String state = "Nebraska";
+      
       System.out.println("Name:" + " " + name + " " + "Age:" + " " + age + " " + "State:" + " " + state);
   }
 }
