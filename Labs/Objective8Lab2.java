40ba91258b89a2255a24a30e213d5e5a317154c8
Kenneth Pederson <kenneth.pederson@yahoo.com>
2022-05-10 12:14:21 -0500

    Complete Objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,12 @@
+public class Objective8Lab2 {
+
+  public static void main(String[] args) {
+    // set a counter variable starting at 0
+    int result = 0;
+    // iterate and add i to the counter each time through up to 20
+    for (int i = 0; i <= 20; i++) {
+      result += i;
+    }
+    System.out.println(result);
+  }
+}
