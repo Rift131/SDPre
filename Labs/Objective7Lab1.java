eef44368cd070c857fad50f296af7d232e8180e7
Kenneth Pederson <kenneth.pederson@yahoo.com>
2022-05-10 10:58:40 -0500

    Complete Objective7Lab1

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,12 @@
+public class Objective7Lab1 {
+
+  public static void main(String [] args) {
+      // set variable to be number to countdown from
+      int counter = 10;
+      // set a while loop to print the number variable and remove 1 from the countdown variable until it is zero
+      while (counter > 0) {
+        System.out.println(counter);
+        counter--;
+      }
+  }
+}
