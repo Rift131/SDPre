d5eae7912899fc0f6c35b802b8fb74a701dcd9ee
Kenneth Pederson <kenneth.pederson@yahoo.com>
2022-05-10 11:04:22 -0500

    Complete Objective7Lab2

---
+++ Labs/Objective7Lab2.java
@@ -0,0 +1,12 @@
+public class Objective7Lab2 {
+
+  public static void main(String[] args) {
+    // establish a counter at zero
+    int counter = 0;
+    // perform a while loop that will print the counter and add 1 to the counter so long as it is less than or equal to 20
+    while (counter <= 20) {
+      System.out.println(counter);
+      counter++;
+    }
+  }
+}
